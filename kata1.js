4045865184f6885913d30c0fd4a4d74f5d14d742
Matthew Johnson <mtthw.jhnson@gmail.com>
2024-01-17 18:45:24 -0600

    Testing environment set up, empty string and single digit string

--- kata1.js
+++ kata1.js
@@ -1,18 +1,10 @@
-function stringCalc(nums){
-    if(nums == ""){
-        return 0;
-    }
-    else {
-       var sum = 0;
-       for(var i=0; i<nums.length; i++){
-           if(!isNaN(nums[i])) {
-               sum += parseInt(nums[i]);
-           }
-       }
-       return sum;
-    }
-}
+function stringCalc(nums) {
+if (nums === ""){
+    return 0
+} 
+    return Number(nums)
+};
 
-stringCalc("1,2,3")
-console.log(stringCalc("1,2,3"))
-module.exports = stringCalc;
+// stringCalc("1,2");
+
+module.exports = stringCalc
--- test.js
+++ test.js
@@ -4,6 +4,14 @@ test("String Calculator Returns 0 for empty string", () => {
   expect(stringCalc("")).toBe(0);
 });
 
-test("String Calculator Returns 6 for 1,2,3", () => {
-  expect(stringCalc("1,2,3")).toBe(6);
-})
+test("String Calcualtor Returns 1", () => {
+  expect(stringCalc("1")).toBe(1)
+});
+
+test("String Calculator Returns 2", () => {
+  expect(stringCalc("2")).toBe(2)
+});
+
+// test("String Calculator Returns 6 for 1,2,3", () => {
+//   expect(stringCalc("1,2,3")).toBe(6);
+// })
